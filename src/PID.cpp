2505c627b7f60c0fc6e1dd530151b46dc93cfa3d
chongrui.wang <wangchongrui2002@163.com>
2024-09-27 01:52:24 +0800

    fix: PID todo

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,6 +10,26 @@
 #include "vex.h"
 using namespace std;
 
+/**
+ * 本文件定义PID控制器
+ * 需完成以下内容并在注释中完整解释：
+ * PID::update()
+ * DirPID::update()
+ * PosPID::update()
+ * 
+ * 样例使用方法：
+ * PID pid;
+ * pid.setTarget(100);
+ * pid.setErrorTolerance(1);
+ * pid.setCoefficient(1, 0.1, 0.1);
+ * while(!pid.taragetArrived()){
+ *      pid.update(curr_value);
+ *      output = pid.getOutput();
+ *      // do something with output
+ * }
+ * 
+ */
+
 PID::PID() : first_time(true), arrived(false), I_max(20), I_range(50), jump_time(50), D_tol(10) { my_timer.reset(); }
 
 void PID::setFirstTime() { first_time = true; }
@@ -31,31 +51,12 @@ double PID::getOutput() { return output; }
 
 void PID::update(double input) {
     error_curt = target - input;  // calculate current error
-    P = kp * error_curt;
-    if (first_time) {  // first time to update
-        first_time = false;
-        error_prev = error_curt;
-        error_int = 0;
-    }
-    error_dev = error_curt - error_prev;  // calculate the derivative of error
-    error_prev = error_curt;              // record error
-    D = kd * error_dev;                   // calculate D
-    if (abs(P) >= I_range) {              // I = 0 for P > I_range
-        error_int = 0;
-    } else {  // P <= I_range -> Integrate
-        error_int += error_curt;
-        if (abs(error_int) * ki > I_max)  // Limit I to I_max
-            error_int = sign(error_int) * I_max / ki;
-    }
-    if (sign(error_int) != sign(error_curt) || (abs(error_curt) <= error_tol))  // Clear I for small enough error
-        error_int = 0;
-    I = ki * error_int;                                     // Calculate I
-    if (abs(error_curt) <= error_tol && abs(D) <= D_tol) {  // Exit when staying in tolerated region and
-                                                            // maintaining a low enough speed for enough time
-        if (my_timer.getTime() >= jump_time) arrived = true;
-    } else {
-        my_timer.reset();
-        arrived = false;
+    //TODO: calculate the contribution of P, I, D with kp, ki, kd
+
+    
+    if (abs(error_curt) <= error_tol) {  // Exit when staying in tolerated region and
+                                        // maintaining a low enough speed
+        arrived = true;
     }
     output = P + I + D;
 }
@@ -65,62 +66,24 @@ void PosPID::setTarget(Point p) { target_point = p; }
 void PosPID::update(Point input) {
     Vector err = target_point - input;
     error_curt = err.mod();  // calculate current error
-    P = kp * error_curt;
-    if (first_time) {  // first time to update
-        first_time = false;
-        error_prev = error_curt;
-        error_int = 0;
-    }
-    error_int = error_curt - error_prev;  // calculate the derivative of error
-    error_prev = error_curt;              // record error
-    D = kd * error_int;                   // calculate D
-    if (abs(P) >= I_range) {              // I = 0 for P > I_range
-        error_int = 0;
-    } else {  // P <= I_range -> Integrate
-        error_int += error_curt;
-        if (abs(error_int) * ki > I_max)  // Limit I to I_max
-            error_int = sign(error_int) * I_max / ki;
-    }
-    if (sign(error_int) != sign(error_curt) || (abs(error_curt) <= error_tol))  // Clear I for small enough error
-        error_int = 0;
-    I = ki * error_int;                                     // Calculate I
-    if (abs(error_curt) <= error_tol && abs(D) <= D_tol) {  // Exit when staying in tolerated region and
-                                                            // maintaining a low enough speed for enough time
-        if (my_timer.getTime() >= jump_time) arrived = true;
-    } else {
-        my_timer.reset();
-        arrived = false;
+    //TODO: calculate the contribution of P, I, D with kp, ki, kd
+
+    
+    if (abs(error_curt) <= error_tol) {  // Exit when staying in tolerated region and
+                                        // maintaining a low enough speed
+        arrived = true;
     }
     output = P + I + D;
 }
 
 void DirPID::update(double input) {
     error_curt = degNormalize(target - input);  // calculate current error
-    P = kp * error_curt;
-    if (first_time) {  // first time to update
-        first_time = false;
-        error_prev = error_curt;
-        error_int = 0;
-    }
-    error_int = error_curt - error_prev;  // calculate the derivative of error
-    error_prev = error_curt;              // record error
-    D = kd * error_int;                   // calculate D
-    if (abs(P) >= I_range) {              // I = 0 for P > I_range
-        error_int = 0;
-    } else {  // P <= I_range -> Integrate
-        error_int += error_curt;
-        if (abs(error_int) * ki > I_max)  // Limit I to I_max
-            error_int = sign(error_int) * I_max / ki;
-    }
-    if (sign(error_int) != sign(error_curt) || (abs(error_curt) <= error_tol))  // Clear I for small enough error
-        error_int = 0;
-    I = ki * error_int;  // Calculate I
-    // Exit when staying in tolerated region and maintaining a low enough speed for enough time
-    if (abs(error_curt) <= error_tol && abs(D) <= D_tol) {
-        if (my_timer.getTime() >= jump_time) arrived = true;
-    } else {
-        my_timer.reset();
-        arrived = false;
+    //TODO: calculate the contribution of P, I, D with kp, ki, kd
+
+    
+    if (abs(error_curt) <= error_tol) {  // Exit when staying in tolerated region and
+                                        // maintaining a low enough speed
+        arrived = true;
     }
     output = P + I + D;
 }
